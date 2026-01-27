60513d4afd453e9cdb6aa499520921b7bef8efc2
Pedro Machado de Paula <pmdpaula@gmail.com>
2026-01-27 19:50:17 -0300

    feat: enhance sign-out functionality with error handling and improved code structure

--- src/app/api/auth/sign-out/route.ts
+++ src/app/api/auth/sign-out/route.ts
@@ -1,12 +1,21 @@
 import { cookies } from "next/headers";
 import { type NextRequest, NextResponse } from "next/server";
 
+import { apiClient } from "@/http/api-client";
+
 export async function GET(request: NextRequest) {
   const redirectUrl = request.nextUrl.clone();
 
   redirectUrl.pathname = "/auth/sign-in";
 
   const cookiesStore = await cookies();
+
+  try {
+    await apiClient.post("users/sessions/sign-out");
+  } catch (error) {
+    console.error("Error signing out from backend:", error);
+  }
+
   cookiesStore.delete("token");
 
   return NextResponse.redirect(redirectUrl);
