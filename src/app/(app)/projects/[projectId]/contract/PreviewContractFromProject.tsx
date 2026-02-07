493271c89b2844fabc631826d45f33b9d12d3ca3
Pedro Machado de Paula <pmdpaula@gmail.com>
2026-02-07 10:33:54 -0300

    feat: enhance project contract page with data fetching and PDF generation
    - Implemented data fetching for project details, cake batters, and fillings using React Query.
    - Refactored contract data structure to include detailed cake information and contractor details.
    - Added loading state with CircularProgress while fetching data.
    - Integrated PDF generation functionality with a button to generate contracts.
    - Displayed warning alert if no contractor is defined for the project.

---
+++ src/app/(app)/projects/[projectId]/contract/PreviewContractFromProject.tsx
@@ -0,0 +1,413 @@
+import { Container, Stack } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography, { type TypographyProps } from "@mui/material/Typography";
+import Image from "next/image";
+
+import logoPSB from "@/assets/psb-banner_bglight.svg";
+import { companyData } from "@/data/companyData";
+
+import type { ContractData } from "./page";
+
+interface TextProps extends TypographyProps {
+  children: React.ReactNode;
+}
+
+interface FieldProps extends TypographyProps {
+  title: string;
+  value: string;
+}
+
+type PreviewContractFromProjectProps = {
+  data: ContractData;
+};
+
+const PreviewContractFromProject = ({
+  data,
+}: PreviewContractFromProjectProps) => {
+  const Title = (props: TextProps) => {
+    return (
+      <Typography
+        variant="body1"
+        sx={{ color: "black", mt: 1.5, mb: 0.5, fontWeight: "500" }}
+      >
+        {props.children}
+      </Typography>
+    );
+  };
+
+  const Field = (props: FieldProps) => {
+    return (
+      <Stack
+        direction="row"
+        my={0.3}
+      >
+        <Typography
+          variant="caption"
+          sx={{ color: "black", fontWeight: "bold" }}
+        >
+          {props.title}
+        </Typography>
+        <Typography
+          variant="caption"
+          sx={{ color: "blue", ml: 1 }}
+        >
+          {props.value}
+        </Typography>
+      </Stack>
+    );
+  };
+
+  return (
+    <Container maxWidth="lg">
+      <Box
+        bgcolor="white"
+        p={0.5}
+      >
+        <Typography
+          variant="caption"
+          color="gray"
+        >
+          página 1
+        </Typography>
+
+        <Box
+          p={1}
+          pb={12}
+          sx={{ border: "2px solid black", borderRadius: 1 }}
+        >
+          <Image
+            src={logoPSB}
+            alt="logotipo com um bolo estilizado rosa e marrom a esquerda e a direita o nome Patricia Siqueira"
+            width={300}
+          />
+          <Title>1. Identificação das Partes</Title>
+
+          <Field
+            title="Contratante:"
+            value={data.contractorName}
+          />
+          <Field
+            title="Contratada:"
+            value={data.companyData}
+          />
+
+          <Typography
+            variant="caption"
+            sx={{ color: "black", my: 1 }}
+          >
+            As partes acima têm entre si acertado o seguinte Contrato de
+            Prestação de Serviços, que se regerá pelas seguintes clausulas e
+            pelas condições de preço, forma e termo de pagamento descritas no
+            presente.
+          </Typography>
+
+          <Title>2. Do Objeto do Contrato</Title>
+
+          <Typography
+            variant="caption"
+            sx={{ color: "black", my: 1 }}
+          >
+            Clausula 1ª: É objeto do presente contrato a confecção do bolo de
+            acordo com a descrição que segue no formulário abaixo:
+          </Typography>
+
+          <Field
+            title="Quantidade de bolos:"
+            value={String(data.cakes.length)}
+          />
+          {data.cakes.map((cake, index) => {
+            return (
+              <Box
+                key={index}
+                bgcolor="grey.100"
+                sx={{ borderRadius: 1, py: 0, px: 0.4, my: 0.1 }}
+              >
+                <Stack
+                  direction="row"
+                  my={1}
+                  spacing={4}
+                >
+                  <Field
+                    title="Nº fatias:"
+                    value={String(cake.slices || "")}
+                  />
+                  <Field
+                    title="Massa do bolo:"
+                    value={cake.batterName}
+                  />
+                </Stack>
+
+                <Field
+                  title="Recheios:"
+                  value={`${cake.filling1Name}, ${cake.filling2Name} e ${cake.filling3Name}`}
+                />
+
+                <Stack
+                  direction="row"
+                  my={1}
+                  spacing={4}
+                >
+                  <Field
+                    title="Andares:"
+                    value={String(cake.tiers)}
+                  />
+
+                  <Field
+                    title="Descrição:"
+                    value={cake.description}
+                  />
+                </Stack>
+              </Box>
+            );
+          })}
+
+          <Stack
+            direction="row"
+            my={1}
+            spacing={4}
+            justifyContent="space-between"
+          >
+            <Field
+              title="Valor:"
+              value={data.fullPrice.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            />
+            <Field
+              title="Sinal:"
+              value={data.allPaymentsMade.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            />
+            <Field
+              title="Saldo:"
+              value={(data.fullPrice - data.allPaymentsMade).toLocaleString(
+                "pt-BR",
+                {
+                  style: "currency",
+                  currency: "BRL",
+                },
+              )}
+            />
+          </Stack>
+
+          <Field
+            title="Forma de pagamento:"
+            value={data.paymentMethod || "a definir"}
+          />
+
+          <Stack
+            direction="row"
+            my={1}
+            spacing={4}
+            justifyContent="space-between"
+          >
+            <Field
+              title="Dados bancários:"
+              value={`${companyData.bankData.bankName} (${companyData.bankData.bankNumber}), ag. ${companyData.bankData.agency}, cc. ${companyData.bankData.account}.`}
+            />
+
+            <Field
+              title="Pix:"
+              value={`${companyData.bankData.pixType} ${companyData.bankData.pix}`}
+            />
+          </Stack>
+
+          <Title>3. Obrigações da Contratante</Title>
+
+          <Typography
+            variant="caption"
+            sx={{ color: "black", my: 1 }}
+          >
+            Clausula 2ª: A CONTRATANTE deverá fornecer a CONTRATADA as
+            informações do formulário seguinte a fim de que a entrega seja feita
+            sem contratempos para ambas as partes.
+          </Typography>
+
+          <Field
+            title="Local da entrega:"
+            value={data.locaName}
+          />
+          <Field
+            title="Data da entrega:"
+            value={data.eventDate}
+          />
+          {/* <Field title="Hora da entrega:" value={data.horaEntrega} /> */}
+
+          <Stack
+            direction="row"
+            my={1}
+            spacing={2}
+            justifyContent="normal"
+          >
+            <Field
+              title="Telefone cliente:"
+              value={data.contractorContact1}
+            />
+            <Field
+              title="Telefone adicional:"
+              value={data.contractorContact2 || ""}
+            />
+          </Stack>
+
+          <Stack
+            direction="row"
+            my={1}
+            spacing={10}
+            justifyContent="normal"
+          >
+            <Field
+              title="Contato do evento:"
+              value={data.plannerName || ""}
+            />
+            <Field
+              title="Telefone contato do evento:"
+              value={data.plannerContact || ""}
+            />
+          </Stack>
+        </Box>
+      </Box>
+
+      <Box
+        bgcolor="white"
+        p={0.5}
+        mt={1.5}
+      >
+        <Typography
+          variant="caption"
+          color="gray"
+        >
+          página 2
+        </Typography>
+
+        <Box
+          p={1}
+          pb={24}
+          sx={{ border: "2px solid black", borderRadius: 1 }}
+        >
+          <Image
+            src={logoPSB}
+            alt="logotipo com um bolo estilizado rosa e marrom a esquerda e a direita o nome Patricia Siqueira"
+            width={300}
+          />
+          <Title>4. Políticas de Cancelamento ou Adiamento</Title>
+
+          <Typography
+            variant="caption"
+            sx={{ color: "black", my: 1 }}
+          >
+            <p>Clausula 3ª: Casos de desistência por parte do contratante:</p>
+            <p>
+              • Em até 60 dias antes da data evento ocorre-se a devolução de 30%
+              do valor pago.
+            </p>
+            <p>
+              • Em até 30 dias antes da data evento ocorre-se a devolução de 20%
+              do valor pago.
+            </p>
+            <p>
+              • Em até 11 dias antes da data evento ocorre-se a devolução de 10%
+              do valor pago.
+            </p>
+            <p>
+              • Do décimo dia anterior a data do evento em diante, não haverá
+              devolução de valor.
+            </p>
+            <p>
+              Clausula 4ª: Para adiamento de data, o valor do produto poderá
+              sofrer reajuste da seguinte forma:
+            </p>
+            <p>• Até 6 meses de adiamento – ajuste de acordo com o IPCA;</p>
+            <p>
+              • Acima de 6 meses, conforme valores de tabela vigente na nova
+              data do evento.
+            </p>
+          </Typography>
+
+          <Title>5. Da Validação do Contrato</Title>
+
+          <Stack spacing={0}>
+            <Typography
+              variant="caption"
+              sx={{ color: "black", my: 0.5 }}
+            >
+              Clausula 5ª: A data somente será reservada mediante pagamento de
+              sinal.
+            </Typography>
+
+            <Typography
+              variant="caption"
+              sx={{ color: "black", my: 0.5 }}
+            >
+              Clausula 6ª: O valor total deverá estar quitado até 7 dias antes
+              da data do evento sob pena de perder a reserva da data.
+            </Typography>
+
+            <Typography
+              variant="caption"
+              sx={{ color: "black", my: 0.5 }}
+            >
+              Clausula final: Se por efeito de força maior, o bolo não puder ser
+              feito pela Patricia Siqueira, será feito a devolução total do
+              valor.
+            </Typography>
+          </Stack>
+
+          <Stack
+            direction="row"
+            mt={6}
+            justifyContent="space-around"
+          >
+            <Typography
+              variant="body2"
+              sx={{ color: "black", my: 0.5 }}
+            >
+              ____________________________
+            </Typography>
+
+            <Typography
+              variant="body2"
+              sx={{ color: "black", my: 0.5 }}
+            >
+              ____________________________
+            </Typography>
+          </Stack>
+
+          <Stack
+            direction="row"
+            justifyContent="space-around"
+            mb={5}
+          >
+            <Typography
+              variant="caption"
+              sx={{ color: "black", fontSize: 10 }}
+            >
+              Assinatura da(o) Contratante
+            </Typography>
+
+            <Typography
+              variant="caption"
+              sx={{ color: "black", fontSize: 10 }}
+            >
+              Assinatura da Contratada
+            </Typography>
+          </Stack>
+
+          <Field
+            title="Data:"
+            value={new Date().toLocaleDateString("pt-BR", {
+              weekday: "long",
+              day: "2-digit",
+              month: "long",
+              year: "numeric",
+            })}
+          />
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default PreviewContractFromProject;
--- src/app/(app)/projects/[projectId]/contract/page.tsx
+++ src/app/(app)/projects/[projectId]/contract/page.tsx
@@ -1,84 +1,273 @@
 "use client";
 
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
-import PreviewIcon from "@mui/icons-material/Preview";
-import { Button, Stack } from "@mui/material";
+import {
+  Alert,
+  Box,
+  Button,
+  CircularProgress,
+  Stack,
+  useMediaQuery,
+} from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
 import { use, useEffect, useState } from "react";
 
+import { companyData } from "@/data/companyData";
 import type { ProjectFullDataDto } from "@/data/dto/project-dto";
+import { getCakeBatters } from "@/http/data-types/get-cake-batters";
+import { getCakeFillings } from "@/http/data-types/get-cake-fillings";
+import { getProjectFullDataById } from "@/http/project/get-project-full-data-by-id";
 
-import { getProjectFullDataByIdAction } from "../../action";
+import { ProjectHeader } from "../../components/ProjectHeader";
+import { generatePdfFromProject } from "./actions";
+import PreviewContractFromProject from "./PreviewContractFromProject";
 
-const fetchFullProjectData = async (projectId: string) => {
-  const projectFullDataInDb = await getProjectFullDataByIdAction(projectId);
+// const fetchFullProjectData = async (projectId: string) => {
+//   const projectFullDataInDb = await getProjectFullDataByIdAction(projectId);
 
-  const data: ProjectFullDataDto = projectFullDataInDb;
-  return data;
-};
+//   const data: ProjectFullDataDto = projectFullDataInDb;
+//   return data;
+// };
 
 interface ProjectContractPageProps {
   params: Promise<{ projectId: string }>;
 }
 
-type _ContractData = {
-  nomeCliente: string[];
-  dadosContratada: string;
-  qtdFatias: string;
-  massaBolo: string;
-  recheio1: string;
-  recheio2: string;
-  recheio3: string;
-  observacoesBolo: string;
-  modeloBolo: string;
-  valorBolo: string;
-  sinalBolo: string;
-  saldoBolo: string;
-  formaPagamentoBolo: string;
-  localEntrega: string;
-  dataEntrega: string;
-  horaEntrega: string;
-  telefoneCliente: string;
-  telefoneAdicional: string;
-  nomeContatoEvento: string;
-  telefoneContatoEvento: string;
+export type ContractData = {
+  contractorName: string;
+  companyData: string;
+  cakes: {
+    description: string;
+    slices: number;
+    tiers: number;
+    price: number;
+    batterName: string;
+    filling1Name: string;
+    filling2Name: string;
+    filling3Name: string;
+  }[];
+  // cakes: CakeDto[];
+  // cakes: ProjectFullDataDto["cakes"];
+  // qtdFatias: string;
+  // massaBolo: string;
+  // recheio1: string;
+  // recheio2: string;
+  // recheio3: string;
+  // observacoesBolo: string;
+  // modeloBolo: string;
+  // valorBolo: string;
+  // sinalBolo: string;
+  // saldoBolo: string;
+  // formaPagamentoBolo: string;
+  locaName: string;
+  eventDate: string;
+  eventHour?: string;
+  contractorContact1: string;
+  contractorContact2?: string;
+  plannerName?: string;
+  plannerContact?: string;
+  fullPrice: number;
+  allPaymentsMade: number;
+  paymentMethod?: string;
 };
 
 const ProjectContractPage = ({ params }: ProjectContractPageProps) => {
   const { projectId } = use(params);
-  const [projectFull, setProjectFull] = useState<ProjectFullDataDto | null>(
-    null,
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+  const [contractData, setContractData] = useState<ContractData | null>(null);
+  const [contractorData, setContractorData] = useState<
+    ProjectFullDataDto["collaboratorsInProject"][0] | null
+  >(null);
+  const [plannerData, setPlannerData] = useState<
+    ProjectFullDataDto["collaboratorsInProject"][0] | null
+  >(null);
+  const [isReadingData, setIsReadingData] = useState(true);
+
+  const { data: projectFull } = useQuery({
+    queryKey: [`projects/${projectId}/full-data`],
+    queryFn: async () => await getProjectFullDataById({ id: projectId }),
+  });
+
+  const { data: cakeBattersData, isLoading: isLoadingCakeBatters } = useQuery({
+    queryKey: ["cake-batters"],
+    queryFn: async () => await getCakeBatters(),
+  });
+
+  const { data: cakeFillingsData, isLoading: isLoadingCakeFillings } = useQuery(
+    {
+      queryKey: ["cake-fillings"],
+      queryFn: async () => await getCakeFillings(),
+    },
   );
 
-  useEffect(() => {
-    const loadProjectFullData = async () => {
-      const data = await fetchFullProjectData(projectId);
-      setProjectFull(data);
-    };
+  function getContractorData(projectData: ProjectFullDataDto) {
+    return (
+      projectData.collaboratorsInProject.find(
+        (collab) => collab.collaboratorType.code === "CONTRACTOR",
+      ) || null
+    );
+  }
 
-    loadProjectFullData();
-  }, [projectId]);
+  function getPlannerData(projectData: ProjectFullDataDto) {
+    return (
+      projectData.collaboratorsInProject.find(
+        (collab) => collab.role === "PLANNER",
+      ) || null
+    );
+  }
+
+  function transformProjectDataToContractData(
+    projectData: ProjectFullDataDto,
+    contractor: ProjectFullDataDto["collaboratorsInProject"][0],
+    planner: ProjectFullDataDto["collaboratorsInProject"][0] | null,
+  ): ContractData {
+    const transformedCakes = projectData.cakes.map((cake) => {
+      const batterName = cakeBattersData!.cakeBatters.find(
+        (batter) => batter.code === cake.batterCode,
+      )!.name;
+
+      const filling1Name = cakeFillingsData!.cakeFillings.find(
+        (filling) => filling.code === cake.fillingCode1,
+      )!.name;
+
+      const filling2Name = cakeFillingsData!.cakeFillings.find(
+        (filling) => filling.code === cake.fillingCode2,
+      )!.name;
+      const filling3Name = cakeFillingsData!.cakeFillings.find(
+        (filling) => filling.code === cake.fillingCode3,
+      )!.name;
+
+      return {
+        description: cake.description,
+        slices: cake.slices,
+        tiers: cake.tiers,
+        price: cake.price,
+        batterName,
+        filling1Name,
+        filling2Name,
+        filling3Name,
+      };
+    });
+
+    return {
+      contractorName: contractor.collaborator.name,
+      companyData: `${companyData.representativeName}, ${companyData.representativeRole}, representante da ${companyData.name}, localizada na ${companyData.address} , inscrita no ${companyData.companyIdType}: ${companyData.companyIdNumber}`,
+      cakes: transformedCakes,
+      // qtdFatias: projectData.cake.slices.toString(),
+      // massaBolo: projectData.cake.batter,
+      // recheio1: projectData.cake.filling1,
+      // recheio2: projectData.cake.filling2 || "",
+      // recheio3: projectData.cake.filling3 || "",
+      // observacoesBolo: projectData.cake.notes || "",
+      // modeloBolo: projectData.cake.model,
+      // valorBolo: projectData.payment.totalValue.toString(),
+      // sinalBolo: projectData.payment.downPayment.toString(),
+      // saldoBolo: (
+      //   projectData.payment.totalValue - projectData.payment.downPayment
+      // ).toString(),
+      // formaPagamentoBolo: projectData.payment.paymentMethod,
+      locaName: projectData.localName || "",
+      eventDate: projectData.eventDate,
+      eventHour: "a definir",
+      contractorContact1: contractor.collaborator.contact1 || "",
+      contractorContact2: contractor.collaborator.contact2 || "",
+      plannerName: plannerData?.collaborator.name || "",
+      plannerContact:
+        planner?.collaborator.contact1 ||
+        planner?.collaborator.contact2 ||
+        planner?.collaborator.contact2 ||
+        "",
+      fullPrice: projectData.cakes.reduce((total, price) => {
+        return total + price.price;
+      }, projectData.shippingCost),
+      allPaymentsMade: projectData.payments.reduce((total, payment) => {
+        return total + payment.amount;
+      }, 0),
+      paymentMethod: projectData.paymentMethod || "",
+    };
+  }
+
+  function handleGeneratePdf() {
+    generatePdfFromProject(contractData!);
+  }
+
+  useEffect(() => {
+    if (projectFull && !isLoadingCakeBatters && !isLoadingCakeFillings) {
+      const contractor = getContractorData(projectFull);
+      const planner = getPlannerData(projectFull);
+
+      setContractorData(contractor);
+      setPlannerData(planner);
+
+      if (contractor) {
+        const transformedData = transformProjectDataToContractData(
+          projectFull,
+          contractor,
+          planner,
+        );
+
+        setContractData(transformedData);
+      } else {
+        setContractData(null);
+      }
+
+      setIsReadingData(false);
+    }
+  }, [projectFull, cakeBattersData, cakeFillingsData]);
 
   return (
     <>
-      <Stack spacing={2}>
-        <Button
-          variant="contained"
-          startIcon={<PictureAsPdfIcon />}
-          // onClick={handleGeneratePdf}
-        >
-          Gerar PDF
-        </Button>
+      {isReadingData ? (
+        <Box sx={{ display: "flex", justifyContent: "center", mt: 4 }}>
+          <CircularProgress color="primary" />
+        </Box>
+      ) : (
+        <>
+          {projectFull && (
+            <>
+              <ProjectHeader
+                projectId={projectId}
+                name={projectFull.name || ""}
+                description={projectFull.description || ""}
+              />
 
-        <Button
-          variant="outlined"
-          startIcon={<PreviewIcon />}
-          // onClick={togglePreview}
-        >
-          Visualizar rascunho
-        </Button>
-      </Stack>
+              <Stack spacing={2}>
+                <Button
+                  variant="contained"
+                  startIcon={<PictureAsPdfIcon />}
+                  fullWidth
+                  disabled={!contractData}
+                  onClick={handleGeneratePdf}
+                >
+                  Gerar PDF
+                </Button>
 
-      {projectFull && <pre>{JSON.stringify(projectFull, null, 2)}</pre>}
+                {contractorData && contractData ? (
+                  <PreviewContractFromProject data={contractData!} />
+                ) : (
+                  <Stack
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="center"
+                    sx={{ mt: 4 }}
+                  >
+                    <Alert
+                      severity="warning"
+                      sx={{ width: "90%" }}
+                    >
+                      O projeto não possui um contratante definido. Não é
+                      possível gerar um contrato.
+                    </Alert>
+                  </Stack>
+                )}
+              </Stack>
+            </>
+          )}
+        </>
+      )}
+
+      {/* {projectFull && <pre>{JSON.stringify(projectFull, null, 2)}</pre>} */}
     </>
   );
 };
